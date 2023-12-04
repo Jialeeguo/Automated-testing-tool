d675e0864a136c92799cc8b7ecc02974013621f9
trookie2000 <trookie2000@163.com>
2023-12-05 01:14:34 +0800

    [修复了只能点击先点击截图才能终止录制的bug]

--- src-tauri/src/record/record_main.rs
+++ src-tauri/src/record/record_main.rs
@@ -108,95 +108,44 @@ pub mod record_main {
             });
         }
 
-        // loop {
-        //     // let should_stop = {
-        //     //     let stop_flag = SHOULD_STOP.lock().unwrap();
-        //     //     *stop_flag
-        //     // };
+        loop {
+            let should_stop = {
+                let stop_flag = SHOULD_STOP.lock().unwrap();
+                *stop_flag
+            };
 
-        //     // if should_stop {
-        //     //     println!("录制被终止");
-        //     //     break;
-        //     // }
-        //     let keys: Vec<Keycode> = device_state.get_keys();
-        // if (keys.len() != 0 && keys[0] == Keycode::F2) || clickButton == true {
-        //         sleep(Duration::from_millis(300));
-        //         //截图事件F2触发
-        //         println!("进入截图功能");
-        //         println!("请等待几秒，正在提取图片文字...");
-        //         {
-        //             let mut screen_flag = SCREEN_SHOT_FLAG.lock().unwrap();
-        //             *screen_flag = true;
-        //         }
-        //         let output = Command::new("C:/Users/trookie/venv/Scripts/textshot")
-        //             .arg("eng+chi_sim")
-        //             .output()
-        //             .expect("无法启动 textshot 命令");
+            if should_stop {
+                println!("录制被终止");
+                break;
+            }
+            let keys: Vec<Keycode> = device_state.get_keys();
+        if (keys.len() != 0 && keys[0] != Keycode::F1) {
+                let duration_key = START_TIME.lock().unwrap().elapsed().as_millis();
+                println!("{}ms,捕捉到键盘输入{:?}", duration_key, keys[0]);
+                let output = format!("{},key,{:?}\n", duration_key, keys[0]);
+                save_file.write_all(output.as_bytes()).unwrap();
+                sleep(Duration::from_millis(175));
+                continue;
+            } else if keys.len() == 0 {
+                continue;
+            }
+            let mut mouse_flag = MOUSE_THREAD_FLAG.lock().unwrap();
 
-        //         if output.status.success() {
-        //             let textshot_time = *TEXTSHOT_ACTION_TIME.lock().unwrap();
+            *mouse_flag = true;
 
-        //             let mut file = OpenOptions::new()
-        //                 .write(true)
-        //                 .create(true)
-        //                 .append(true)
-        //                 .open(format!(
-        //                     "../Automated-testing/result/{}/textshot_{}.txt",
-        //                     now_dir,
-        //                     textshot_time.to_string()
-        //                 ))
-        //                 .expect("无法打开文件");
+            if status == "started"{
+                println!(
+                    "\n经过了：{}毫秒",
+                    START_TIME.lock().unwrap().elapsed().as_millis()
+                );
+                // std::thread::sleep(std::time::Duration::from_secs(3));
+                // mouse_stop_flag = true;
+                // println!("flag当前是3：{}", mouse_stop_flag);
 
-        //             let output_confirm = String::from_utf8_lossy(&output.stdout);
-        //             //判断是否没有文字，如果是就不写入了
-        //             if output_confirm
-        //                 .contains("ERROR: Unable to read text from image, did not copy")
-        //             {
-        //             } else {
-        //                 // let output_bytes = &output.stdout;
-        //                 // let output_without_info = &output_bytes[14..output_bytes.len() - 20];
-        //                 let mut output_without_info: ClipboardContext =
-        //                     ClipboardProvider::new().unwrap();
-        //                 let mut clipboard_content = output_without_info.get_contents().unwrap();
-        //                 clipboard_content.push('\n');
-        //                 file.write_all(clipboard_content.as_bytes())
-        //                     .expect("提取文字命令写入文件失败");
-        //             }
-
-        //             println!("提取文字执行成功！请继续操作。");
-        //         } else {
-        //             let error = String::from_utf8_lossy(&output.stderr);
-        //             println!("提取文字命令执行失败: {}", error);
-        //         }
-
-        //         continue;
-        //     } else if (keys.len() != 0 && keys[0] != Keycode::F1) {
-        //         let duration_key = START_TIME.lock().unwrap().elapsed().as_millis();
-        //         println!("{}ms,捕捉到键盘输入{:?}", duration_key, keys[0]);
-        //         let output = format!("{},key,{:?}\n", duration_key, keys[0]);
-        //         save_file.write_all(output.as_bytes()).unwrap();
-        //         sleep(Duration::from_millis(175));
-        //         continue;
-        //     } else if keys.len() == 0 {
-        //         continue;
-        //     }
-        //     let mut mouse_flag = MOUSE_THREAD_FLAG.lock().unwrap();
-
-        //     *mouse_flag = true;
-
-        //     if status == "started"{
-        //         println!(
-        //             "\n经过了：{}毫秒",
-        //             START_TIME.lock().unwrap().elapsed().as_millis()
-        //         );
-        //         // std::thread::sleep(std::time::Duration::from_secs(3));
-        //         // mouse_stop_flag = true;
-        //         // println!("flag当前是3：{}", mouse_stop_flag);
-
-        //         // handle.join().unwrap();
-        //         return;
-        //     }
-        // }
+                // handle.join().unwrap();
+                return;
+            }
+        }
     }
     //点击终止录制后函数
     #[tauri::command]
